f72d982d1047ad87307e94d1307050197274141d
calvin cushman and daniel lopez <calvin@>
2017-05-11 09:42:50 -0700

    testing hold function

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,15 @@
 /////////////////////////////////////////////////
 
 // constructors
-function Player (name, score,) {
+function Game (turn, playTo) {
+  this.turn = turn;
+  this.playTo = playTo;
+}
+
+function Player (name, score) {
   this.name = name;
   this.score = score;
+
 }
 
 function Dice (sides) {
@@ -17,7 +23,21 @@ Dice.prototype.roll = function (result) {
   return result;
 };
 
+Game.prototype.passTurn = function () {
+  if (this.turn === true) {
+    this.turn = false;
+  } else {
+    this.turn = true;
+  }
+}
 
+Game.prototype.pushScore = function (turnScore) { debugger;
+  if (this.turn === true) {
+    this.score.push(turnScore);
+  } else {
+    this.score.push(turnScore);
+  }
+}
 $(function() {
 
   $("form#entry").submit(function(event) {
@@ -36,6 +56,9 @@ $(function() {
 
     // make objects
     var die = new Dice(diceType);
+    var player1 = new Player(player1name, 0, true)
+    var player2 = new Player(player2name, 0, false)
+    var newGame = new Game(true, playTo);
 
     // displays
     $("#playToDisplay").text(playTo);
@@ -45,17 +68,28 @@ $(function() {
     // add image to result display
     $(".result-div").prepend("<img src='img/" + diceType + ".png'>");
 
+    // roll button
     $("#rollButton").click(function(event) {
       var result = die.roll();
         if (result === 1) {
           alert("You rolled a 1.")
           turnScore = 0;
+          newGame.passTurn();
+          alert(newGame.turn);
         } else {
           turnScore += result;
         }
       $("#turnscore-display").text(turnScore);
-
       $("#result-display").text(result);
+    });
+
+    //hold button
+    $("#holdButton").click(function(event) {
+      newGame.passTurn();
+      alert(newGame.turn);
+      newGame.pushScore();
+      alert(player1.score);
+      alert(player2.score);
     });
   });
 
